5016cf87dff9293134e000c3e5d62a563010e39b
Vasiliy Mikhailov <mikhailov@emociono.com>
2018-01-18 23:44:56 +0300

    auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import 'core-js';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
-import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +11,6 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   ngOnInit() {
-    firebase.initializeApp({
-      apiKey: "AIzaSyCjRqVX4lNFnOnMF7CYSY08Bc2VVYRR_mo",
-      authDomain: "transit2-0.firebaseapp.com"
-    });
   }
 
   constructor(public authService: AuthService) {
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,37 +1,35 @@
-import * as firebase from 'firebase';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../environments/environment';
+import { Md5 } from 'ts-md5/dist/md5';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
   token: string;
+  email: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private httpClient: HttpClient) {
   }
 
   signInWithEmailAndPassword(email: string, password: string) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(response => {
-        firebase.auth().currentUser.getIdToken()
-          .then(
-            (token: string) => {
-              this.token = token;
-            }
-          );
+    const apiUrl = environment.apiUrl;
 
-        this.router.navigate(['']);
-      })
-      .catch((err) => console.log(err));
+    this.httpClient.get<any>(apiUrl + '/login.json?email=' + email + '&password_md5=' + Md5.hashStr(password))
+      .subscribe(
+        (response: any) => {
+          this.token = response.token;
+          this.email = response.email;
+
+          this.router.navigate(['']);
+        }
+      );
   }
 
   signOut() {
-    firebase.auth().signOut()
-      .then(response => {
-        this.token = null;
-
-        this.router.navigate(['']);
-      })
-      .catch((err) => console.log(err));
+    this.token = null;
+    this.email = null;
+    this.router.navigate(['']);
   }
 
   getToken() {
@@ -52,7 +50,7 @@ export class AuthService {
   }
 
   getUserEMail(): String {
-    return firebase.auth().currentUser.email;
+    return this.email;
   }
 
 }
